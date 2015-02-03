a8d9563aa0b41d166fe47d54895adfa03e1d168f
normington <normington@gmail.com>
2015-02-03 13:56:42 +0000

    Adding training section
    Also tidied up old files.

---
+++ training/index.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+orientEar by Andy Normington
+Copyright (c) 2015
+See LICENSE.txt
+*/
+
+
+// UserCake authentication
+require_once("../models/config.php");
+
+// Request method: GET
+$ajax = checkRequestMode("get");
+
+if (!securePage(__FILE__)){
+    apiReturnError($ajax);
+}
+
+
+setReferralPage(getAbsoluteDocumentPath(__FILE__));
+
+
+// Training index page
+?>
+
+<!doctype html>
+<html lang="en">
+<?php
+    echo renderAccountPageHeader(array("#SITE_ROOT#" => SITE_ROOT, "#SITE_TITLE#" => SITE_TITLE, "#PAGE_TITLE#" => "Admin Dashboard"));
+?>
+
+<head>
+    <title>Training | orientEar</title>
+</head>
+
+<body>
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php
+            echo renderMenu("training");
+        ?>
+
+        <div id="page-wrapper">
+
+            <h2>The Training Page</h2>
+
+
+        </div><!-- /#page-wrapper -->
+
+    </div><!-- /#wrapper -->
+
+</body>
+
+</html>
