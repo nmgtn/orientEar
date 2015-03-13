7ec053e1664beca542a1d817acf6548c29e61146
normington <normington@gmail.com>
2015-03-13 12:25:13 +0000

    Wait for document ready before populating divs

--- training/templates/matchEQ.js
+++ training/templates/matchEQ.js
@@ -1,10 +1,7 @@
 // Load AudioContext
 var context = new AudioContext();
 
-// Load WaveSurfer
-var wavesurfer = loadWaveSurfer('./test.wav', 'waveform', context);
-
-// Do some filters
+// Filter specs
 var EQ = [
     {
         freq: 200,
@@ -26,19 +23,27 @@ var EQ = [
         type: 'highshelf'
     }
 ];
-var filterArray = createGraphicEQ(EQ, 40, 'graphic-EQ', context);
 
-// Pass filters to wavesurfer (incl workaround for dual paths)
-connectNodesToWaveSurfer(filterArray, wavesurfer);
+// Give document a chance to load before populating divs
+$(document).ready(function () {
+    // Load WaveSurfer
+    var wavesurfer = loadWaveSurfer('./test.wav', 'waveform', context);
 
-// Toggle the transform on or off
-var transformActive = true;
-function toggleTransform() {
-    if(transformActive) {
-        wavesurfer.backend.setFilters();
-        transformActive = false;
-    } else {
-        connectNodesToWaveSurfer(filterArray, wavesurfer);
-        transformActive = true;
+    // Create filers
+    var filterArray = createGraphicEQ(EQ, 40, 'graphic-EQ', context);
+
+    // Pass filters to wavesurfer (incl workaround for dual paths)
+    connectNodesToWaveSurfer(filterArray, wavesurfer);
+
+    // Toggle the transform on or off
+    var transformActive = true;
+    function toggleTransform() {
+        if(transformActive) {
+            wavesurfer.backend.setFilters();
+            transformActive = false;
+        } else {
+            connectNodesToWaveSurfer(filterArray, wavesurfer);
+            transformActive = true;
+        }
     }
-}
+});
